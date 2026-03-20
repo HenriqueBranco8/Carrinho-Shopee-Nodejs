5934d70d1b95e0d200f149d5f5d56150bd723ca5
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-03-20 19:00:02 -0300

    Fazendo a lógica do carrinho e dos itens

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hellow')
+import createItem from './services/item.js'
+import * as cartService from './services/cart.js'
+
+
+const myCart = []
+const item1 = await createItem('Book', 19.99, 1)
+const item2 = await createItem('smartphone', 899.99, 2)
+
+
+console.log('Welcome to the your White Cart!')
+
+await cartService.additem(myCart, item1)
+//await cartService.additem(myCart, item2)
+
+console.log(item2.subtotal())
+
+console.log('total your card is: ')
+await cartService.caculatorTotal(myCart)
---
+++ src/services/cart.js
@@ -0,0 +1,39 @@
+//Quais ações meu carrinho pode fazer
+
+
+//Funcionalidades:
+
+//Adicionar um item 🆗
+const additem = async (userCart, item) => {
+    userCart.push(item)
+}
+
+//Deleter um item (Isso vai deletar o item indepedente da quantide, vai excluir tudo)
+const deleteItem = async (userCart, nameItem) => {
+
+}
+
+//Diminuir um item(Vai apenas diminuir a quantide do item que está no carrinho)
+const reduceItem = async (userCart, index) => {
+
+}
+
+//Acrescentar um item
+const increaseItem = async (userCart, nameItem) => {
+
+}
+
+const caculatorTotal = async (userCart) => {
+    const result = userCart.reduce((total, item)=> total + item.subtotal(), 0)
+    console.log(result)
+}
+//Mostrar valor fixo do item
+//Total do carrinho
+
+export {
+    additem,
+    deleteItem,
+    reduceItem,
+    increaseItem,
+    caculatorTotal
+}
